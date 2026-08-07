94f59aaf0a55668f3a062e4b586480e5aee29fa3
TY <TY@mail.com>
2026-08-07 12:56:35 +0900

    login結合テスト作成

--- tests/integration/test_integration_login.py
+++ tests/integration/test_integration_login.py
@@ -68,22 +68,49 @@ def test_login_empty(driver, base_url):
 
     time.sleep(2)
 
+
 """
 整合性エラー
 """
 # passwordが違う時
-def test_login_success(driver, base_url, test_user):
+def test_login_wrong_password(driver, base_url, test_user):
     url = f"{base_url}/login"
 
     driver.get(url)
 
     email = test_user["email"]
-    password = test_user["password"]
 
     driver.find_element(
             By.NAME,
             "email"
         ).send_keys(email)
+
+    driver.find_element(
+            By.NAME,
+            "password"
+        ).send_keys("password")
+    
+    time.sleep(2)
+
+    driver.find_element(
+            By.CSS_SELECTOR,
+            "button[type='submit']"
+        ).click()
+
+    time.sleep(2)
+
+# メールアドレスが違う時
+def test_login_wrong_email(driver, base_url, test_user):
+    url = f"{base_url}/login"
+
+    driver.get(url)
+
+    password = test_user["password"]
+
+    driver.find_element(
+            By.NAME,
+            "email"
+        ).send_keys("email@email.com")
 
     driver.find_element(
             By.NAME,
@@ -98,10 +125,6 @@ def test_login_success(driver, base_url, test_user):
         ).click()
 
     time.sleep(2)
-
-    # ログイン画面へ遷移確認
-    assert "/login" in driver.current_url
-
 
 
 # 新規登録ボタン
