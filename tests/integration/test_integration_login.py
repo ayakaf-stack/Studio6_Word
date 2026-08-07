e8e152b5b5a9bd7a5c1763b041f79ce1b33078ae
TY <TY@mail.com>
2026-08-07 12:33:54 +0900

    結合テスト(login) Fixes #123

---
+++ tests/integration/test_integration_login.py
@@ -0,0 +1,123 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+# ログイン画面表示
+def test_login_page(driver, base_url):
+    url = f"{base_url}/login"
+
+    driver.get(url)
+
+    time.sleep(1)
+
+    title = driver.find_element(By.TAG_NAME,"h1").text
+
+    assert title == "ログイン"
+
+
+# ログイン成功
+"""
+ログイン画面を開きemailとpasswordを入力してログインボタンクリックでマイページへ遷移
+"""
+def test_login_success(driver, base_url, test_user):
+    url = f"{base_url}/login"
+
+    driver.get(url)
+
+    email = test_user["email"]
+    password = test_user["password"]
+
+    driver.find_element(
+            By.NAME,
+            "email"
+        ).send_keys(email)
+
+    driver.find_element(
+            By.NAME,
+            "password"
+        ).send_keys(password)
+    
+    time.sleep(2)
+
+    driver.find_element(
+            By.CSS_SELECTOR,
+            "button[type='submit']"
+        ).click()
+
+    time.sleep(2)
+
+    # マイページ画面へ遷移確認
+    assert "/mypage" in driver.current_url
+
+
+# ログイン失敗
+"""
+空欄エラー
+"""
+def test_login_empty(driver, base_url):
+    url = f"{base_url}/login"
+
+    driver.get(url)
+
+    time.sleep(1)
+
+    driver.find_element(
+            By.CSS_SELECTOR,
+            "button[type='submit']"
+        ).click()
+
+    time.sleep(2)
+
+"""
+整合性エラー
+"""
+# passwordが違う時
+def test_login_success(driver, base_url, test_user):
+    url = f"{base_url}/login"
+
+    driver.get(url)
+
+    email = test_user["email"]
+    password = test_user["password"]
+
+    driver.find_element(
+            By.NAME,
+            "email"
+        ).send_keys(email)
+
+    driver.find_element(
+            By.NAME,
+            "password"
+        ).send_keys(password)
+    
+    time.sleep(2)
+
+    driver.find_element(
+            By.CSS_SELECTOR,
+            "button[type='submit']"
+        ).click()
+
+    time.sleep(2)
+
+    # ログイン画面へ遷移確認
+    assert "/login" in driver.current_url
+
+
+
+# 新規登録ボタン
+"""
+ログイン画面を開き新規登録ボタンクリックで新規登録画面へ遷移
+"""
+def test_login_register(driver, base_url):
+    url = f"{base_url}/login"
+
+    driver.get(url)
+
+    time.sleep(2)
+
+    register = driver.find_element(By.LINK_TEXT, "新規登録")
+    assert register.text == "新規登録"
+    # 新規登録クリック
+    register.click()
+
+    time.sleep(2)
